e0f47d895003660435e7e11eb9486f4bbf568941
Fynn <fynn@fynnyoung.com>
2025-11-06 16:55:26 +0200

    rejig setting prev values

--- controller-hands.py
+++ controller-hands.py
@@ -23,7 +23,7 @@ else:
     print("opening virtual port")
     midiout.open_virtual_port("My virtual output")
 
-def send_midi(corrected_note: int, previous_corrected_note: int, clamped_volume: float, previous_clamped_volume: int) -> int:
+def send_midi(corrected_note: int, previous_corrected_note: int, clamped_volume: float, previous_clamped_volume: int):
     normalised_volume = (1 - clamped_volume) * 127
 
     if previous_corrected_note != corrected_note:
@@ -32,7 +32,6 @@ def send_midi(corrected_note: int, previous_corrected_note: int, clamped_volume:
 
     if previous_clamped_volume != normalised_volume:
         midiout.send_message([AFTERTOUCH_CH1, normalised_volume])
-    return corrected_note
 
 def get_corrected_note(clamped_pitch: float, left_hand_landmarks: list, scale: list) -> int:
     base_note = round(1 - clamped_pitch, 1) * 10 + 60
@@ -153,7 +152,10 @@ with mp_hands.Hands(
 
             if thumb_x < 0.06:
                 corrected_note = get_corrected_note(clamped_pitch, left_wrist_landmarks, MAJOR_SCALE_INTERVALS)
-                previous_corrected_note = send_midi(corrected_note, previous_corrected_note, clamped_volume, previous_clamped_volume)
+                send_midi(corrected_note, previous_corrected_note, clamped_volume, previous_clamped_volume)
+                
+                previous_corrected_note = corrected_note
+                previous_clamped_volume = clamped_volume
                 draw_note_name(corrected_note, frame)
             else:
                 midiout.send_message([ALL_OFF_CH1, 123, 0])
